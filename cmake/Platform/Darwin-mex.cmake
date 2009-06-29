db1e91301c7561d1dd9fcf9d7d1de8a48f2cfd9b
Matt McCormick (thewtex) <matt@mmmccormick.com>
2009-06-29 11:23:05 -0500

    remove cmake hacks and only leave tests
    cannot just adjust MODULE_PATH because of use of ${CMAKE_ROOT} with
    *Configure.in files

--- cmake/Platform/Darwin-mex.cmake
+++
@@ -1,4 +0,0 @@
-set(CMAKE_SHARED_LIBRARY_SUFFIX ".mexaci")
-set(CMAKE_SHARED_MODULE_SUFFIX  ".mexaci")
-
-include(Platform/mex)
--- cmake/Platform/SunOS-mex.cmake
+++
@@ -1,4 +0,0 @@
-set(CMAKE_SHARED_LIBRARY_SUFFIX ".mexs64")
-set(CMAKE_SHARED_MODULE_SUFFIX  ".mexs64")
-
-include(Platform/mex)
